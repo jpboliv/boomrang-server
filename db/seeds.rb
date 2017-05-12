63d6e66286724e7b7ab16bdab1dc032c710938a8
João Pedro Baptista Oliveira <joao@hobbes.lan>
2017-05-12 15:05:40 +0100

    default songs and moods added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-troll = Mood.create(name:"Troll")
-pump = Mood.create(name:"Pump-me-up")
-chill = Mood.create(name:"Chill")
+#Moods default
 random = Mood.create(name:"Random")
+troll = Mood.create(name:"Troll")
+party = Mood.create(name:"Party")
+relax = Mood.create(name:"Relax")
+gloomy = Mood.create(name:"Gloomy")
+funky = Mood.create(name:"Funky")
 
+#Music default
+Music.create(link:"https://www.youtube.com/watch?v=fe4EK4HSPkI", mood: random)
+Music.create(link:"https://www.youtube.com/watch?v=Q46lu56oO4k", mood: random)
+Music.create(link:"https://www.youtube.com/watch?v=gKb-GYBFxGU", mood: random)
+Music.create(link:"https://www.youtube.com/watch?v=mUnMOG8xKsg", mood: random)
+Music.create(link:"https://www.youtube.com/watch?v=lmc21V-zBq0", mood: random)
 
-Music.create(link:"https://www.youtube.com/watch?v=dQw4w9WgXcQ", mood: troll)  #troll
-Music.create(link:"https://www.youtube.com/watch?v=0hhz7KSEIAE", mood: troll)  #troll
-Music.create(link:"https://www.youtube.com/watch?v=4zLfCnGVeL4", mood: chill) #depressing
-Music.create(link:"https://www.youtube.com/watch?v=ZaI2IlHwmgQ", mood: pump) #Pump-me-up
-Music.create(link:"https://www.youtube.com/watch?v=mUnMOG8xKsg", mood: random) #random
+Music.create(link:"https://www.youtube.com/watch?v=dQw4w9WgXcQ", mood: troll)
+Music.create(link:"https://www.youtube.com/watch?v=BsFlyUtICyA", mood: troll)
+Music.create(link:"https://www.youtube.com/watch?v=9jK-NcRmVcw", mood: troll)
+Music.create(link:"https://www.youtube.com/watch?v=feA64wXhbjo", mood: troll)
+Music.create(link:"https://www.youtube.com/watch?v=hAShEnz1glk", mood: troll)
+
+Music.create(link:"https://www.youtube.com/watch?v=IcrbM1l_BoI", mood: party)
+Music.create(link:"https://www.youtube.com/watch?v=O3c4dPxN1qM", mood: party)
+Music.create(link:"https://www.youtube.com/watch?v=x84m3YyO2oU", mood: party)
+Music.create(link:"https://www.youtube.com/watch?v=MV_3Dpw-BRY", mood: party)
+Music.create(link:"https://www.youtube.com/watch?v=2Q1Y1O9DBhQ", mood: party)
+
+Music.create(link:"https://www.youtube.com/watch?v=jfFTT3iz740", mood: relax)
+Music.create(link:"https://www.youtube.com/watch?v=rVeMiVU77wo", mood: relax)
+Music.create(link:"https://www.youtube.com/watch?v=1R0AUyvNc4Y", mood: relax)
+Music.create(link:"https://www.youtube.com/watch?v=dX3k_QDnzHE", mood: relax)
+Music.create(link:"https://www.youtube.com/watch?v=Pib8eYDSFEI", mood: relax)
+
+Music.create(link:"https://www.youtube.com/watch?v=vt1Pwfnh5pc", mood: gloomy)
+Music.create(link:"https://www.youtube.com/watch?v=u7K72X4eo_s", mood: gloomy)
+Music.create(link:"https://www.youtube.com/watch?v=YQHsXMglC9A", mood: gloomy)
+Music.create(link:"https://www.youtube.com/watch?v=IXdNnw99-Ic", mood: gloomy)
+Music.create(link:"https://www.youtube.com/watch?v=4zLfCnGVeL4", mood: gloomy)
+
+Music.create(link:"https://www.youtube.com/watch?v=OPf0YbXqDm0", mood: funky)
+Music.create(link:"https://www.youtube.com/watch?v=AoATQO82k-k", mood: funky)
+Music.create(link:"https://www.youtube.com/watch?v=I_izvAbhExY", mood: funky)
+Music.create(link:"https://www.youtube.com/watch?v=sFvENQBc-F8", mood: funky)
+Music.create(link:"https://www.youtube.com/watch?v=NF-kLy44Hls", mood: funky)
